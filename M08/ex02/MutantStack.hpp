baf5efa6bfd29b3f29db5824387008c95e928350
Laura Massonet <lamasson@2D3.42angouleme.fr>
2023-12-01 21:13:29 +0100

    ddjd

--- M08/ex02/MutantStack.hpp
+++ M08/ex02/MutantStack.hpp
@@ -6,24 +6,31 @@
 /*   By: lamasson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 18:04:57 by lamasson          #+#    #+#             */
-/*   Updated: 2023/11/30 21:03:44 by lamasson         ###   ########.fr       */
+/*   Updated: 2023/12/01 21:02:20 by lamasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
 
-#include <iterator>
 #include<stack>
 
 template<typename T>
 class MutantStack : public std::stack<T> {
+	
 	public:
-		MutantStack<T>(void);
-		~MutantStack<T>(void);
 		
-		class	iterator : public std::iterator<> {
-			public:
-		};
+		MutantStack<T>() : std::stack<T>() {}
+		MutantStack<T>(const MutantStack<T> &src) : std::stack<T>(src) {
+			*this = src;
+		}
+		MutantStack<T>&	operator=(const MutantStack<T> &rhs) {
+			if (this != &rhs)
+				this = rhs.c->operator=(rhs);
+			return (*this);
+		}
+		~MutantStack<T>();
+	
+		typedef MutantStack<T>::std::stack<T>::
 
 	private:
 
